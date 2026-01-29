546a609abe5d75400d8c168e134526945cb911c5
Taehoon Kange <kang0493@umn.edu>
2026-01-29 15:13:45 -0600

    Initial Commit

---
+++ Dutchie/DutchieApp.swift
@@ -0,0 +1,17 @@
+//
+//  DutchieApp.swift
+//  Dutchie
+//
+//  Created by Taehoon Kang on 1/29/26.
+//
+
+import SwiftUI
+
+@main
+struct DutchieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
